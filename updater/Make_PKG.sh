fe981a4e7abcddc425be21ad31b8670ac081ea80
freddy38510 <freddyescobar@hotmail.fr>
2016-12-06 18:22:13 +0100

    Clean code and printed informations

--- updater/Make_PKG.sh
+++ updater/Make_PKG.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 
-#[ -f EP0001-UPDWEBMOD_00-0000000000000000.pkg ] && rm EP0001-UPDWEBMOD_00-0000000000000000.pkg || :
-#[ -f webMAN_MOD_1.45.xx_Updater.pkg ] && rm webMAN_MOD_1.45.xx_Updater.pkg || :
-
-make clean
+[ -f EP0001-UPDWEBMOD_00-0000000000000000.pkg ] && rm EP0001-UPDWEBMOD_00-0000000000000000.pkg || :
+[ -f webMAN_MOD_1.45.xx_Updater.pkg ] && rm webMAN_MOD_1.45.xx_Updater.pkg || :
 
 echo -e "1. Standard icons"
 echo -e "2. KMZ icons"
  
 tput bold
 read -p "Enter your choice [1-2] " choice
- 
 tput clear
-
 tput sgr0
 
 [ $choice = "1" ] && 
 cp -R icons/standard/* pkgfiles/USRDIR/; echo -ne "\033]0;Building pkg with standard icons\007" || 
 cp -R icons/kzm/* pkgfiles/USRDIR/; echo -ne "\033]0;Building pkg with KMZ icons\007"
 
-#echo -ne "\033]0;Building pkg with...\007"
-
+make clean
 make pkg
 
 mv build/pkg build/EP0001-UPDWEBMOD_00-0000000000000000
@@ -35,9 +30,10 @@ echo "ContentType = GameExec" >>package.conf
 wine psn_package_npdrm.exe -n package.conf "build\EP0001-UPDWEBMOD_00-0000000000000000" 2>&1 
 
 rm package.conf
-
 [ -f webMAN_MOD_1.43.xx_Updater.pkg ] && rm webMAN_MOD_1.43.xx_Updater.pkg || :
 [ -f webMAN_MOD_1.45.xx_Updater.pkg ] && rm webMAN_MOD_1.45.xx_Updater.pkg || :
-mv EP0001-UPDWEBMOD_00-0000000000000000.pkg webMAN_MOD_1.45.xx_Updater.pkg
 rm -rf build
+
+mv EP0001-UPDWEBMOD_00-0000000000000000.pkg webMAN_MOD_1.45.xx_Updater.pkg
+ls -o ../webMAN_MOD*.pkg
 echo -ne "\033]0;Finished\007"
