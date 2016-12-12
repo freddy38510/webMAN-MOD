2e9642d0fd3a69a720c8bc42290630c2c1de9099
freddy38510 <freddyescobar@hotmail.fr>
2016-12-12 15:22:55 +0100

    Fix script to compile ps3netsrv on Linux

--- ps3netsrv/Make.sh
+++ ps3netsrv/Make.sh
@@ -1,3 +1,6 @@
-mv Makefile Makefile.win
-mv Makefile.bak Makefile
+#!/bin/sh
+if [ "$(uname)" == "Linux" ]; then
+	[ -f Makefile.bak ] && (mv Makefile Makefile.win; mv Makefile.bak Makefile;) || :
+fi
+make clean
 make
