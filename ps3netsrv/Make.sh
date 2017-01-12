3c664cb4a7091ab3f0b9f45b96c7379dab417ac6
freddy38510 <freddyescobar@hotmail.fr>
2017-01-12 15:17:49 +0100

    Fix Make.sh of ps3netsrv and update sprx files

--- ps3netsrv/Make.sh
+++ ps3netsrv/Make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$(uname)" == "Linux" ]; then
-	[ -f Makefile.bak ] && (mv Makefile Makefile.win; mv Makefile.bak Makefile;) || :
+	[ -f Makefile.bak ] && (mv Makefile Makefile.win; mv Makefile.bak Makefile) || :
 fi
 make clean
 make
